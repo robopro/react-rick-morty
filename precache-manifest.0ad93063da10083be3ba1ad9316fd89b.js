863bb3be2d848503f8b931612171116651b9327f
Morten Salvesen <msalvefirm@gmail.com>
2020-03-05 11:13:35 +0100

    Deploying to gh pages

---
+++ precache-manifest.0ad93063da10083be3ba1ad9316fd89b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fd9fde0cdab8f41592d0112ba63ae4",
+    "url": "/react-rick-morty/index.html"
+  },
+  {
+    "revision": "0b9d0180ccc5f5dd8855",
+    "url": "/react-rick-morty/static/css/main.6083b439.chunk.css"
+  },
+  {
+    "revision": "eb8876edd2d0a0a80dc3",
+    "url": "/react-rick-morty/static/js/2.a08ea507.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-rick-morty/static/js/2.a08ea507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b9d0180ccc5f5dd8855",
+    "url": "/react-rick-morty/static/js/main.c7db87f4.chunk.js"
+  },
+  {
+    "revision": "3d9f2aac6591115fc698",
+    "url": "/react-rick-morty/static/js/runtime-main.a8e7ae10.js"
+  },
+  {
+    "revision": "1d645dd02dc5f6aaaa494ae2f80691bb",
+    "url": "/react-rick-morty/static/media/logo.1d645dd0.png"
+  }
+]);
